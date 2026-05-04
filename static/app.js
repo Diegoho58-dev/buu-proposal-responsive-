46ee374c77025825ed72f4182b4f59f05e670386
Diegoho <diegoho@unicauca.edu.co>
2026-05-04 11:37:24 -0500

    Update app.js

--- static/app.js
+++ static/app.js
@@ -2,17 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const textareas = document.querySelectorAll('textarea');
 
   textareas.forEach((textarea) => {
-    const resizeTextarea = () => {
+    textarea.addEventListener('input', () => {
       textarea.style.height = 'auto';
       textarea.style.height = `${textarea.scrollHeight}px`;
-    };
-
-    resizeTextarea();
-    textarea.addEventListener('input', resizeTextarea);
+    });
   });
-
-  const messagesContainer = document.getElementById('waMessages');
-  if (messagesContainer) {
-    messagesContainer.scrollTop = messagesContainer.scrollHeight;
-  }
 });
