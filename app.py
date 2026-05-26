baeff97efa20b78d639390c5639d89b6f953a7fc
Diegoho <diegoho@unicauca.edu.co>
2026-05-26 12:12:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import pytz
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 import json
